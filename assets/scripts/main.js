6fa610a2e0b99f937678d8df3464f604e59bfd6c
David Morales <dmorales@Davids-MacBook-Pro.local>
2022-10-13 16:21:00 -0700

    Add search bar and albums page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -16,6 +16,32 @@ $().ready(function() {
 
 });
 
+/**
+ * Adopted from SO solution here: https://stackoverflow.com/questions/13639464/javascript-equivalent-to-pythons-format
+ * Allows python-like formatting of strings to accomodate re-usable string templates
+ */
+String.prototype.format = function() {
+  var args = arguments;
+  this.unkeyed_index = 0;
+  return this.replace(/\{(\w*)\}/g, function(match, key) { 
+    if (key === '') {
+      key = this.unkeyed_index;
+      this.unkeyed_index++
+    }
+    if (key == +key) {
+      return args[key] !== 'undefined'
+      ? args[key]
+      : match;
+    } else {
+      for (var i = 0; i < args.length; i++) {
+        if (typeof args[i] === 'object' && typeof args[i][key] !== 'undefined') {
+          return args[i][key];
+        }
+      }
+      return match;
+    }
+  }.bind(this));
+};
 
 function setMainContent(navName) {
     $('#main-content').load('/assets/html/' + navName +'.html');
