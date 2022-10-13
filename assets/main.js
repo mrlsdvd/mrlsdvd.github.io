533b98700a9d9e16aed71d3e6b5ada6539553d16
David Morales <dmorales@Davids-MacBook-Pro.local>
2022-10-12 22:38:26 -0700

    Show about content as default

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,22 @@
 $().ready(function() {
    /**
+    * Begin with about content as default
+    */
+    updateContent('about')
+    
+   /**
     * Add listeners to the beginning of the page creation 
     */
     // Navigation listeners
     $(".nav-link").click(function() {
         $(".nav-link").removeClass('active');
         $(this).addClass('active');
-        updateContent($(this).attr('id'))
+        updateContent($(this).attr('id').split('-').pop())
     });
 
 });
 
 
 function updateContent(nav_name) {
-    $('#main-content').load('/assets/' + nav_name.split('-').pop() +'.html');
+    $('#main-content').load('/assets/' + nav_name +'.html');
 }
