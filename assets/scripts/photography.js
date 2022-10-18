f612d5bae26582cdcd3ad97a90b0e2f3558ffdcc
David Morales <dmorales@Davids-MacBook-Pro.local>
2022-10-17 18:01:33 -0700

    Add dummy config for testing

--- assets/scripts/photography.js
+++ assets/scripts/photography.js
@@ -2,6 +2,9 @@ $().ready(function() {
    /**
     * Begin with desired content as default
     */
+    // Load config data
+    var obj = JSON.parse(JSON.stringify('/config.json'))
     console.log("Photography page loaded!")
+    console.log(obj.photography)
 
 });
