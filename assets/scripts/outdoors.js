38136ce4dadf09b67cd1eb8a336fbf13104cd7ca
David Morales <dmorales@Davids-MacBook-Pro.local>
2022-10-19 17:35:25 -0700

    Fix missing variables

--- assets/scripts/outdoors.js
+++ assets/scripts/outdoors.js
@@ -12,24 +12,26 @@ $().ready(function() {
         let sortedEntryInfo = data.outdoors.posts.sort(getObjectComparator('date'), true);
 
         sortedEntryInfo.forEach((postInfo, idx) => {
-            markdownInfo = data.data.markdown[postInfo.markdownId];
+            let markdownInfo = data.data.markdown[postInfo.markdownId];
             let postTitle = postInfo.title;
             let postId = postInfo.id;
             $("#outdoors-outline").append(`<a class="log-outline-item" id="log-outline-item-${postId}">${postTitle}</a>`)
         });
         //Show most recent post
-        let recentPostId = sortedEntryInfo[0].postId;
-        focusOnPost(recentPostId);
+        let recentPostInfo = sortedEntryInfo[0];
+        focusOnPost(recentPostInfo);
 
     });
 });
 
 
-function focusOnPost(postID) {
+function focusOnPost(postInfo) {
     // Validate that postID exists
     // TODO: When adding pagination, entry will not exist yet, so will need to load
     // Set URL to mirror post
     // Open post modal
+
+    let markdownInfo = data.data.markdown[postInfo.markdownId];
     // Load  entry HTML template:
     $.get(OUTDOORS_LOG_ENTRY_COMPONENT_PATH, function(logTemplate) {
         entryHTML = constructOutdoorEntryHTML(logTemplate, postInfo, markdownInfo);
