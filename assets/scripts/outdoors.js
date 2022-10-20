9eb8604b64809b79dd71640631fa633f90d42630
David Morales <dmorales@Davids-MacBook-Pro.local>
2022-10-19 21:15:22 -0700

    Add outdoor outline actions

--- assets/scripts/outdoors.js
+++ assets/scripts/outdoors.js
@@ -17,33 +17,47 @@ $().ready(function() {
             let postId = postInfo.id;
             $("#outdoors-outline").append(`<a class="list-group-item log-outline-item" id="log-outline-item-${postId}">${postTitle}</a>`)
         });
+        // Apply listener to each outline item to focus on
+        $(".log-outline-item").click(function(){
+            let postId = $(this).attr('id').split('-').pop();
+            focusOnPost(postId);
+        });
         //Show most recent post
-        let recentPostInfo = sortedEntryInfo[0];
-        focusOnPost(recentPostInfo, data);
+        let recentPostId = sortedEntryInfo[0].id;
+        focusOnPost(recentPosId);
 
     });
 });
 
 
-function focusOnPost(postInfo, data) {
-    // Validate that postID exists
-    // TODO: When adding pagination, entry will not exist yet, so will need to load
-    // Set URL to mirror post
-    // Open post modal
-
-    var markdownInfo = data.data.markdown[postInfo.markdownId];
-    var postId = postInfo.id;
-    // Load  entry HTML template:
-    $.get(OUTDOORS_LOG_ENTRY_COMPONENT_PATH, function(logTemplate) {
-        console.log(markdownInfo);
-        console.log(postId);
-        entryHTML = constructOutdoorEntryHTML(logTemplate, postInfo, markdownInfo);
-        console.log(entryHTML);
-        $("#outdoors-entry-col").html(entryHTML);
-        // Highlight post in outline
-        $(".log-outline-item").removeClass('active');
-        $("#log-outline-item-"+postId).addClass('active');
+function focusOnPost(postId) {
+    $.getJSON("/config.json", function(data) {
+        let sortedEntryInfo = data.outdoors.posts.sort(getObjectComparator('date', true));
+        var postFound = false;
+        sortedEntryInfo.forEach((postInfo, idx) => {
+            if (postInfo.id == postId) {
+                postFound = true
+                let markdownInfo = data.data.markdown[postInfo.markdownId];
+                let postTitle = postInfo.title;
+                let postId = postInfo.id;
+                // Load  entry HTML template:
+                $.get(OUTDOORS_LOG_ENTRY_COMPONENT_PATH, function(logTemplate) {
+                    console.log(markdownInfo);
+                    console.log(postId);
+                    entryHTML = constructOutdoorEntryHTML(logTemplate, postInfo, markdownInfo);
+                    console.log(entryHTML);
+                    $("#outdoors-entry-col").html(entryHTML);
+                    // Highlight post in outline
+                    $(".log-outline-item").removeClass('active');
+                    $("#log-outline-item-"+postId).addClass('active');
+                });
+            }
+            if (!postFound) { alert("Id not found") }
+            let recentPostId = sortedEntryInfo[0].id;
+            focusOnPost(recentPosId);
+        });
     });
+    
 }
 
 function getPost(postId) {
