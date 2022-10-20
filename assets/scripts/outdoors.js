fcb92383617813805f97785eb29edac5abb7188a
David Morales <dmorales@Davids-MacBook-Pro.local>
2022-10-19 17:30:04 -0700

    Update how and when template is loaded

--- assets/scripts/outdoors.js
+++ assets/scripts/outdoors.js
@@ -5,38 +5,39 @@ $().ready(function() {
    /**
     * Begin with desired content as default
     */
-    // Load photo entry HTML template:
-    $.get(OUTDOORS_LOG_ENTRY_COMPONENT_PATH, function(logTemplate) {
-        // Load config data
-        $.getJSON("/config.json", function(data) {
-            // Sort objects by date
-            let sortedEntryInfo = data.outdoors.posts.sort(getObjectComparator('date'), true);
+    
+    // Load config data
+    $.getJSON("/config.json", function(data) {
+        // Sort objects by date
+        let sortedEntryInfo = data.outdoors.posts.sort(getObjectComparator('date'), true);
 
-            sortedEntryInfo.forEach((postInfo, idx) => {
-                markdownInfo = data.data.markdown[postInfo.markdownId];
-                let postTitle = postInfo.title;
-                let postId = postInfo.id;
-                $("#outdoors-outline").append(`<a class="log-outline-item" id="log-outline-item-${postId}">${postTitle}</a>`)
-            });
-            //Show most recent post
-            let recentPostId = sortedEntryInfo[0].postId;
-            focusOnPost(recentPostId);
-
+        sortedEntryInfo.forEach((postInfo, idx) => {
+            markdownInfo = data.data.markdown[postInfo.markdownId];
+            let postTitle = postInfo.title;
+            let postId = postInfo.id;
+            $("#outdoors-outline").append(`<a class="log-outline-item" id="log-outline-item-${postId}">${postTitle}</a>`)
         });
-    });
+        //Show most recent post
+        let recentPostId = sortedEntryInfo[0].postId;
+        focusOnPost(recentPostId);
 
+    });
 });
+
 
 function focusOnPost(postID) {
     // Validate that postID exists
     // TODO: When adding pagination, entry will not exist yet, so will need to load
     // Set URL to mirror post
     // Open post modal
-    entryHTML = constructOutdoorEntryHTML(logTemplate, postInfo, markdownInfo);
-    $("#outdoors-entry-col").html(entryHTML);
-    // Highlight post in outline
-    $(".log-outline-item").removeClass('active');
-    $("#log-outline-item-${postId}").addClass('active');
+    // Load  entry HTML template:
+    $.get(OUTDOORS_LOG_ENTRY_COMPONENT_PATH, function(logTemplate) {
+        entryHTML = constructOutdoorEntryHTML(logTemplate, postInfo, markdownInfo);
+        $("#outdoors-entry-col").html(entryHTML);
+        // Highlight post in outline
+        $(".log-outline-item").removeClass('active');
+        $("#log-outline-item-${postId}").addClass('active');
+    });
 }
 
 function getPost(postId) {
