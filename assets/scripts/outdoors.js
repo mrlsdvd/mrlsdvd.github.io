b9cbe79e4388fac6b80de34f688896ee6306ad1c
David Morales <dmorales@Davids-MacBook-Pro.local>
2022-10-19 20:24:26 -0700

    Use anchors instead of list items

--- assets/scripts/outdoors.js
+++ assets/scripts/outdoors.js
@@ -15,7 +15,7 @@ $().ready(function() {
             let markdownInfo = data.data.markdown[postInfo.markdownId];
             let postTitle = postInfo.title;
             let postId = postInfo.id;
-            $("#outdoors-outline").append(`<li class="list-group-item log-outline-item" id="log-outline-item-${postId}"><a>${postTitle}</a></li>`)
+            $("#outdoors-outline").append(`<a class="list-group-item log-outline-item" id="log-outline-item-${postId}">${postTitle}</a>`)
         });
         //Show most recent post
         let recentPostInfo = sortedEntryInfo[0];
